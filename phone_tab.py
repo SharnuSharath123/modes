838585cf74798d8719c2278add7ae319587f041b
SharnuSharath123 <sharnuburli143@gmail.com>
2024-07-06 19:37:53 +0530

    modes

---
+++ phone_tab.py
@@ -0,0 +1,25 @@
+# Approach 1
+import phone
+import tab
+
+phone.wire()
+phone.button()
+phone.phones()
+
+tab.wire()
+tab.button
+tab.tabs()
+
+print("=========================================")
+# Approach 2
+from phone import *
+
+wire()
+button()
+phones()
+
+from tab import *
+
+wire()
+button()
+tabs()
